4308c168ca2150acefa1bbdd7c9599b1f7482142
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2021-06-17 11:27:55 +0530

    SendPromo

--- app/Http/Controllers/Frontend/SendMailController.php
+++ app/Http/Controllers/Frontend/SendMailController.php
@@ -3,15 +3,39 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\EmailBuilk;
 use Illuminate\Http\Request;
-use App\Mail\SendEmailPromo;
+use App\Jobs\SendPromoMailJob;
+use DB;
 
 class SendMailController extends Controller
 {
     public function index()
     {
-        $send_mail = 'testlaravel@yopmail.com';
-        new SendEmailPromo($send_mail);
-        dd('send mail successfully !!');
+        dispatch(new SendPromoMailJob('ssss'));
+
+
+
+//
+
+//        EmailBuilk::chunk(100, function ($emailbulk) {
+//            foreach ($emailbulk as $emailbulks)
+//            {
+//                dispatch(new SendPromoMailJob($emailbulks));
+//            }
+//        });
+//        dd('send mail successfully !!');
+
+
+    }
+
+    public static function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
