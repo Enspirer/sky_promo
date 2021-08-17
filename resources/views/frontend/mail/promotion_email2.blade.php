76425e56c8501949264907677443dfb543b573c5
sanjayaharshana <sanjayaharasana@gmail.com>
2021-08-17 19:48:41 +0530

    Promotion Blades

--- resources/views/frontend/mail/promotion_email2.blade.php
+++ resources/views/frontend/mail/promotion_email2.blade.php
@@ -1,3 +1,4 @@
+
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!--[if gte mso 9]>
     <xml>
